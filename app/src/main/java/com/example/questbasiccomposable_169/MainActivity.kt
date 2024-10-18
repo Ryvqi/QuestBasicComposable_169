e58168eb8905e2274987376f3148219434fcfe8a
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-18 14:28:52 +0700

    fix dp line 38

--- app/src/main/java/com/example/questbasiccomposable_169/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_169/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.questbasiccomposable_169.ui.theme.Questbasiccomposable_169Theme
 
 class MainActivity : ComponentActivity() {
