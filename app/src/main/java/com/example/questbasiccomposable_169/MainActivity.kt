8a95a8beeb48e5c52797cfa52ee309f628c4232b
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-18 14:33:41 +0700

    add new import and delete edge to edge

--- app/src/main/java/com/example/questbasiccomposable_169/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_169/MainActivity.kt
@@ -3,21 +3,26 @@ package com.example.questbasiccomposable_169
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_169.ui.theme.Questbasiccomposable_169Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Questbasiccomposable_169Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
